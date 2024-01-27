03e143dae427a3cbadfe1385ca0c158493ff9db2
Hadiseh1383 <157885088+Hadiseh1383@users.noreply.github.com>
2024-01-27 19:03:00 +0330

    Update کنفرانس مهم.py
    This is claened code

--- کنفرانس مهم.py
+++ کنفرانس مهم.py
@@ -1,10 +1,13 @@
 n = int(input())
 domains = set()
+
 for _ in range(n):
-	email = input()
-	if "@" in email:
-		domain = email.split('@')[1]
-		domains.add(domain)
-result=sorted(domains)
+    email = input()
+    if "@" in email:
+        domain = email.split('@')[1]
+        domains.add(domain)
+
+result = sorted(domains)
+
 for item in result:
-	print(item)
+    print(item)
